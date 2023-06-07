54f162e15007e14b5c405c6458b010c9c9961701
germanmalykh <malykh.gn@esphere.ru>
2023-06-07 11:26:21 +0300

    init commit

--- src/test/java/browserstack/sample/tests/SearchWikiTest.java
+++ src/test/java/browserstack/sample/tests/SearchWikiTest.java
@@ -1,21 +1,32 @@
 package browserstack.sample.tests;
 
 import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.$$;
 
 import static com.codeborne.selenide.Selenide.$;
+import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
+import static io.qameta.allure.Allure.step;
 
+@DisplayName("Browserstack test")
 public class SearchWikiTest extends TestBase {
 
+    @DisplayName("Тест на поиск")
     @Test
     void successfulWikiSearchTest() {
-        $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("java");
-        $$(id("org.wikipedia.alpha:id/page_list_item_title"))
-                .shouldHave(sizeGreaterThan(0));
+        step("Переходим к поиску", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+        });
+        step("Вводим слово для поиска", () -> {
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("java");
+        });
+        step("Проверяем, что результаты поиска не равны нулю", () -> {
+            $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                    .shouldHave(sizeGreaterThan(0));
+        });
     }
 }
